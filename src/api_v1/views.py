4222de0bcd967280ee2d9bc055bf96c8fa5f0ed6
tassaron <rutachigal@gmail.com>
2022-03-20 00:13:39 -0400

    extra action for listing a many-to-one relationship

--- src/api_v1/views.py
+++ src/api_v1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
 from rest_framework.reverse import reverse_lazy
 from rest_framework.response import Response
 from .models import Character
@@ -10,30 +11,51 @@ from .serializers import UserSerializer, CharacterSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    Create a new user account by sending a POST request. Email is optional. Password will be re-hashed if changed in a PUT/PATCH request
-    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsOwnerOrReadOnly]
 
     def list(self, request):
+        """Create a new user account by sending a POST request. Email is optional."""
         all_data = []
         for row in self.queryset:
             serializer = self.serializer_class(row, context={'request': request})
             data = serializer.data
             del data["password"]
             del data["email"]
+            data.update({
+                "characters": reverse_lazy('v1:user-characters', args=[row.id], request=request)
+            })
             all_data.append(data)
         return Response(all_data)
 
     def retrieve(self, request, pk=None):
+        """Password will be re-hashed if changed in a PUT/PATCH request"""
         user = get_object_or_404(User, pk=pk)
         serializer = self.serializer_class(user, context={'request': request})
         data = serializer.data
         del data["password"]
         del data["email"]
+        data.update({
+            "characters": reverse_lazy('v1:user-characters', args=[pk], request=request)
+        })
         return Response(data)
+
+    @action(detail=True)
+    def characters(self, request, *args, **kwargs):
+        """Character sheets created by this user"""
+        obj = self.get_object()
+        queryset = Character.objects.filter(user_id=obj.id).all()
+        all_data = []
+        for row in queryset:
+            serializer = CharacterSerializer(row, context={'request': request})
+            data = serializer.data
+            del data["user_id"]
+            data.update({
+                "url": reverse_lazy('v1:character-detail', args=[row.id], request=request)
+            })
+            all_data.append(data)
+        return Response(all_data)
 
 
 class CharacterViewSet(viewsets.ModelViewSet):
@@ -49,8 +71,10 @@ class CharacterViewSet(viewsets.ModelViewSet):
         for row in self.queryset:
             serializer = self.serializer_class(row, context={'request': request})
             data = serializer.data
+            del data["user_id"]
             data.update({
-                "url": reverse_lazy('v1:character-detail', args=[row.id], request=request)
+                "user": reverse_lazy('v1:user-detail', args=[row.user_id], request=request),
+                "url": reverse_lazy('v1:character-detail', args=[row.id], request=request),
             })
             all_data.append(data)
         return Response(all_data)
@@ -60,8 +84,10 @@ class CharacterViewSet(viewsets.ModelViewSet):
         serializer = self.serializer_class(character, context={'request': request})
         data = serializer.data
         data.update({
+            "user": reverse_lazy('v1:user-detail', args=[data["user_id"]], request=request),
             "url": reverse_lazy('v1:character-detail', args=[pk], request=request)
         })
+        del data["user_id"]
         return Response(data)
 
     def create(self, request):
